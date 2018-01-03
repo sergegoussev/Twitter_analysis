7bd48743152b1a126756188d0d5fd9d285cd5e75
Serge Goussev <goussev.serge@gmail.com>
2018-01-02 22:44:16 -0500

    Update CREATE_aggregate_views.sql

--- SQL/CREATE_aggregate_views.sql
+++ SQL/CREATE_aggregate_views.sql
@@ -23,29 +23,29 @@ CREATE VIEW VG_Tweets AS
 SELECT 
 	tt.twtid,
 	tt.userid AS user_id,
-    tu.username AS user_handle,
-    tu.givenname AS user_name,
-    tu.user_lang,
-    tg.geo AS user_geo,
-    tun.numfriends AS user_numfriends,
+	tu.username AS user_handle,
+	tu.givenname AS user_name,
+	tu.user_lang,
+	tg.geo AS user_geo,
+	tun.numfriends AS user_numfriends,
 	tun.numfollowers user_numfollowers,
 	tun.time_stamp AS userinfo_last_updated,
 	tt.twttext,
 	tt.twt_createdat,
 	tt.twt_lang,
-    tn.favcount,
+	tn.favcount,
 	tn.rtcount
 FROM twt_master tt
 	INNER JOIN twt_numbers tn
 	ON tt.twtid=tn.twtid
 	
-    LEFT JOIN users_master tu
+	LEFT JOIN users_master tu
 	ON tt.userid=tu.userid
 	
-    LEFT JOIN users_geo tg
+	LEFT JOIN users_geo tg
 	ON tt.userid=tg.userid
 	
-    LEFT JOIN users_numbers tun
+	LEFT JOIN users_numbers tun
 	ON tt.userid=tun.userid;
 
 
@@ -56,15 +56,15 @@ DROP VIEW VG_Most_rted_twts;
 CREATE VIEW VG_Most_rted_twts AS
 SELECT 
 	rttwtid AS twtid,
-    username,
-    twttext,
+	username,
+	twttext,
 	COUNT(twt_rtmaster.twtid) AS num_times_rted_in_sample
 FROM twt_rtmaster
 	INNER JOIN twt_master
-    ON twt_rtmaster.rttwtid=twt_master.twtid
-    
-    INNER JOIN users_master
-    ON twt_master.userid=users_master.userid
+	ON twt_rtmaster.rttwtid=twt_master.twtid
+	
+	INNER JOIN users_master
+	ON twt_master.userid=users_master.userid
     
 GROUP BY rttwtid
 ORDER BY num_times_rted_in_sample DESC;
@@ -76,28 +76,28 @@ CREATE VIEW VG_quotes AS
 SELECT
 	tm1.twtid,
 	tm1.userid,
-    um1.username,
+	um1.username,
 	tm1.twttext,
 	tm1.twt_lang,
 	tm1.twt_createdat,
 	tm2.twtid AS quoted_twtid,
 	tm2.userid AS quoted_userid,
-    um2.username AS quoted_username,
+	um2.username AS quoted_username,
 	tm2.twttext AS quoted_twttext,
 	tm2.twt_lang AS quoted_lang,
 	tm2.twt_createdat AS quoted_twt_createdat
 FROM twt_qtmaster qt
 	INNER JOIN twt_master tm1
-    ON qt.twtid=tm1.twtid
-    
-    LEFT JOIN users_master um1
-    ON tm1.userid=um1.userid
-    
-    INNER JOIN twt_master tm2
-    ON qt.qttwtid=tm2.twtid
-    
-    LEFT JOIN users_master um2
-    ON tm2.userid=um2.userid;
+	ON qt.twtid=tm1.twtid
+	
+	LEFT JOIN users_master um1
+	ON tm1.userid=um1.userid
+	
+	INNER JOIN twt_master tm2
+	ON qt.qttwtid=tm2.twtid
+	
+	LEFT JOIN users_master um2
+	ON tm2.userid=um2.userid;
 
 
 /*----------administrative views---------------*/
@@ -108,7 +108,7 @@ DROP VIEW VD_TotalDailyRate;
 CREATE VIEW VD_TotalDailyRate AS
 SELECT
 	DATE(twt_createdat) AS dat,
-    COUNT(twtid)
+	COUNT(twtid)
 FROM (
 	SELECT 
 		twt_createdat,
@@ -121,11 +121,11 @@ FROM (
 	FROM twt_rtmaster
 	) AS twts_union
 WHERE DATE(twt_createdat) > (
-							SELECT 
-								DATE(date_start)
-							FROM sample_summary
-								WHERE id=1
-							)
+				SELECT 
+					DATE(date_start)
+				FROM sample_summary
+				WHERE id=1
+			)
 GROUP BY dat;
 
 /*this view creates a view that is constantly updated with today's twtids
@@ -137,6 +137,7 @@ CREATE VIEW VDS_todays_twtids AS
 SELECT
 	tm.twtid
 FROM twt_master tm
+
 WHERE DATE(tm.twt_createdat) = CURDATE();
 
 /*As above, constantly updated list of userids and twtids of today's mentions
@@ -147,7 +148,7 @@ DROP VIEW VDS_todays_mention_meta;
 CREATE VIEW VDS_todays_mention_meta AS
 SELECT
 	mentions.userid,
-    todays_mentions.twtid AS twtid
+	todays_mentions.twtid AS twtid
 FROM (
 	SELECT
 		tm.twtid
@@ -156,7 +157,7 @@ FROM (
 	) AS todays_mentions
 
 	INNER JOIN meta_mentionsmaster mentions
-    ON todays_mentions.twtid=mentions.twtid;
+	ON todays_mentions.twtid=mentions.twtid;
     
 /*--View to describe which users are the most mentioned today--
 NOTE: this view is dependent on VADG vies to function, must be created consecutievely*/
